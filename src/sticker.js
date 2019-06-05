d82ca1018a59ac5364d0d81d33d039eee2bb39cd
Sven Röttering <sven.roettering@10m.de>
2019-06-05 20:38:46 +0200

    Remove non convex images from demo

--- src/sticker.js
+++ src/sticker.js
@@ -3,16 +3,13 @@ const template = document.createElement('template');
 
 template.innerHTML = `
   <style>
-    .sticker > svg > *:first-child {
-      fill: white !important;
-    }
-
     .sticker ::slotted(svg), .sticker ::slotted(img) {
-      filter: drop-shadow(3px 0px 0px rgba(255, 255, 255, 1)) 
-              drop-shadow(0px 3px 0px rgba(255, 255, 255, 1)) 
-              drop-shadow(0px -3px 0px rgba(255, 255, 255, 1)) 
+      width: 100%;
+      filter: drop-shadow(3px 0px 0px rgba(255, 255, 255, 1))
+              drop-shadow(0px 3px 0px rgba(255, 255, 255, 1))
+              drop-shadow(0px -3px 0px rgba(255, 255, 255, 1))
               drop-shadow(-3px 0px 0px rgba(255, 255, 255, 1))
-              drop-shadow(1px 1px 2px rgba(0, 0, 0, .3)) 
+              drop-shadow(1px 1px 2px rgba(0, 0, 0, .3))
               drop-shadow(-1px -1px 2px rgba(0, 0, 0, .3));
     }
   </style>
@@ -30,7 +27,7 @@ class Sticker extends HTMLElement {
 
   connectedCallback() {
     if (!this.shadowRoot) {
-        window.ShadyCSS && window.ShadyCSS.styleElement(this);
+      window.ShadyCSS && window.ShadyCSS.styleElement(this);
       this.attachShadow({ mode: 'open' });
       this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
